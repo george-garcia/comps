1ca061b26e55ad6adde6928de7f78ed639f1a0e7
george-garcia <jorge.a.garcia12@gmail.com>
2022-12-09 13:30:40 -0600

    finished accordion
    accordion section is finished, moving on now

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ function App() {
 
     const items = [
         {
+            id: 'asdjhf',
             label: 'this is the first label',
             content: 'You can do whatever you want. You can do whatever you want. You can do whatever you want. You can do whatever you want.'
         },
         {
+            id: 'asdhfk',
             label: 'this is the second label',
             content: 'You can do whatever you want. You can do whatever you want. You can do whatever you want. You can do whatever you want.'
         },
         {
+            id: 'fasjfdjfsk',
             label: 'this is the third label',
             content: 'You can do whatever you want. You can do whatever you want. You can do whatever you want. You can do whatever you want.'
         }
--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,5 +1,34 @@
-function Accordion(){
-    return <div></div>
+import {useState} from "react";
+import {GoChevronDown, GoChevronLeft} from 'react-icons/go';
+
+function Accordion({items}){
+    const [expandedIndex, setExpandedIndex] = useState(0);
+
+    const handleClick = (index) => {
+        if(expandedIndex === index)
+            setExpandedIndex(-1);
+        else
+            setExpandedIndex(index);
+    }
+
+    const renderedItems = items.map((item, index) => {
+        const isExpanded = index === expandedIndex;
+
+        const icon = <span>
+            {isExpanded ? <GoChevronDown/> : <GoChevronLeft/>}
+        </span>;
+
+        return (
+            <div key={item.id}>
+                <div className="flex justify-between p-3 bg-gray-100 border-b items-center cursor-pointer" onClick={() => handleClick(index)} >
+                    {item.label}
+                    {icon}
+                </div>
+                {isExpanded && <div className="border-b p-5">{item.content}</div>}
+            </div>
+        );
+    })
+    return <div className="border-x border-y">{renderedItems}</div>
 }
 
 export default Accordion;
